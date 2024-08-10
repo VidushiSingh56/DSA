df1643c3365f49a41666b812f38a3aa55d2903fb
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-08-10 17:23:38 +0530

    Time: 0 ms (100%), Space: 42.6 MB (69.17%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,39 +1,55 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) 
     {
-        int count=0;
-        int x=1;
-      while(count<k)
-      {
-        if(!search(arr, x))
+    //     int count=0;
+    //     int x=1;
+    //   while(count<k)
+    //   {
+    //     if(!search(arr, x))
+    //     {
+    //         count++;
+    //     }
+    //     x++;
+
+
+    //   }
+    //   return x-1;
+
+    int min=0, max= arr.length-1;
+    while(min<=max)
+    {
+        int mid = (min+max)/2;
+        int missing =arr[mid]-(mid+1);
+        if(missing < k)
         {
-            count++;
+            min = mid+1;
+            
         }
-        x++;
-
-
-      }
-      return x-1;
+        else{
+            max = mid-1;
+        }
+    }
+    return max+k+1;
       
     }
-    public boolean search(int arr[], int x)
-    {
-        int min=0, max=arr.length-1;
-        while(min<=max)
-        {
-            int mid=(min+max)/2;
-            if(arr[mid]==x)
-            return true;
+    // public boolean search(int arr[], int x)
+    // {
+    //     int min=0, max=arr.length-1;
+    //     while(min<=max)
+    //     {
+    //         int mid=(min+max)/2;
+    //         if(arr[mid]==x)
+    //         return true;
 
-            else if(arr[mid]>x)
-            {
-                max=mid-1;
-            }
-            else
-            {
-                min = mid+1;
-            }
-        }
-        return false;
-    }
+    //         else if(arr[mid]>x)
+    //         {
+    //             max=mid-1;
+    //         }
+    //         else
+    //         {
+    //             min = mid+1;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
