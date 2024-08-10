119f640e455f990a9cce5d9d556698eac35aa9c8
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-08-10 15:58:10 +0530

    Time: 10 ms (91.23%), Space: 47.1 MB (52.89%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -14,7 +14,7 @@ class Solution {
         }
         
         int min=greatest, max=sum;
-        int rem=0;
+        
         while(min<=max)
         {
             int mid=(min+max)/2;
@@ -22,15 +22,14 @@ class Solution {
             
             if(ans <= days)
             {
-                rem = mid;
+                
                 max=mid-1;
             }
             else{
                 min=mid+1;
             }
         }
-        return rem;
-        
+        return min;
     }
     public int func(int weights[], int mid)
     {
