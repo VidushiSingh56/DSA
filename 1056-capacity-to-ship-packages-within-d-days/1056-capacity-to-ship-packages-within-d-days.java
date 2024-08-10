09596fb38bbccb17f51d8e118b8ea1e7d475876d
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-08-10 15:54:07 +0530

    Time: 21 ms (5.1%), Space: 47.3 MB (33.07%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days)
+    {
+        int greatest=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<weights.length;i++)
+        {
+            sum += weights[i];
+            if(greatest < weights[i])
+            {
+                greatest = weights[i];
+            }
+            
+        }
+        System.out.println(greatest);
+         System.out.println(sum);
+        int min=greatest, max=sum;
+        int rem=0;
+        while(min<=max)
+        {
+            int mid=(min+max)/2;
+            int ans=func(weights, mid);
+            System.out.println(ans + "p");
+            if(ans <= days)
+            {
+                rem = mid;
+                max=mid-1;
+            }
+            else{
+                min=mid+1;
+            }
+        }
+        return rem;
+        
+    }
+    public int func(int weights[], int mid)
+    {
+        int sum=0,count=0;
+        for(int i=0;i<weights.length;i++)
+        {
+            sum += weights[i];
+            if(sum>mid)
+            {
+                count++;
+                sum = weights[i];
+            }
+        }
+        count++;
+        return count;
+    }
+}
