26ee8e512ebb6752cca32264c1d43a769f6731ef
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-08-22 00:48:11 +0530

    Time: 6 ms (58.1%), Space: 45.4 MB (97.32%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,29 +3,46 @@ class Solution {
     {
         int m=matrix.length;
         int n=matrix[0].length;
-    for(int i=0;i<m;i++)
-    {
-        if(matrix[i][0]<=target && matrix[i][n-1]>=target)
+    // for(int i=0;i<m;i++)
+    // {
+    //     if(matrix[i][0]<=target && matrix[i][n-1]>=target)
+    //     {
+    //         int low=0,high=n-1;
+    //         while(low<=high)
+    //         {
+    //             int mid=(low+high)/2;
+    //             if(matrix[i][mid]==target)
+    //             {
+    //                 return true;
+    //             }
+    //             else if(matrix[i][mid]<target)
+    //             {
+    //                 low=mid+1;
+    //             }
+    //             else{
+    //                 high=mid-1;
+    //             }
+    //         }
+    //     }
+
+    // }
+        int row=0,col=n-1;
+        while(row<m && col>=0)
         {
-            int low=0,high=n-1;
-            while(low<=high)
+            if(matrix[row][col]==target)
+            return true;
+
+            else if(matrix[row][col]>target)
             {
-                int mid=(low+high)/2;
-                if(matrix[i][mid]==target)
-                {
-                    return true;
-                }
-                else if(matrix[i][mid]<target)
-                {
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                col--;
+            }
+            else
+            {
+                row++;
             }
         }
+    
 
-    }
     return false;
 
     }
