18c3d895609e889644cee2c95c1b24a72cb1f909
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-03 18:19:22 +0530

    Time: 1 ms (64.31%), Space: 41.4 MB (47.38%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -7,29 +7,16 @@ class Solution {
         sum = s.charAt(0)=='(' ? 1 : -1;
         for(int i=1;i<s.length();i++)
         {
-           
             if(s.charAt(i) == '(' && s.charAt(i-1) == ')')
             {
-                
                 ans=(sum>=0)? ans : ans+(int)Math.abs(sum)  ;
                 sum = sum>0 ? sum : 0;
             }
-            // if(s.charAt(i) == ')' && s.charAt(i-1) == '(' && sum==0)
-            // ans=0;
             if(s.charAt(i) == '(')
-            {
-                sum+=1;
-            }
+            sum+=1;
             else
-            {
-                sum-=1;
-            }
-            
-
-            System.out.println(sum);
+            sum-=1;
         }
-        
-        System.out.println(sum);
         ans =ans + (int)Math.abs(sum);
         return ans;
     }
