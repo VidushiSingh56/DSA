35a5b146e110cdbbe63d1183c43958b7c29bb34b
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-03 14:20:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.59%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,32 @@
 class Solution {
+    private Map<Integer, String> letters = Map.of(
+        2, "abc", 3, "def", 4, "ghi", 5, "jkl", 
+        6, "mno", 7, "pqrs", 8, "tuv", 9, "wxyz");
     public List<String> letterCombinations(String digits) 
     {
-        char st[][] = {{},{},{'a','b','c'},{'d','e','f'},
-                        {'g','h','i'},{'j','k','l'},{'m','n','o'},
-                        {'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+        
         List<String> ans = new ArrayList<>();
-        String sub = "";
-        find(digits, 0, st, ans, sub);
+       StringBuilder sub = new StringBuilder();
+        find(digits, 0, ans, sub);
         return ans;
     }
-    public void find(String digits, int i, char st[][], List<String> ans, String sub)
+    public void find(String digits, int i,  List<String> ans, StringBuilder sub)
     {
         if(digits.length()==0)
         return;
 
-        if(i==digits.length())
-        {
-            ans.add(sub);
+        if (sub.length() == digits.length()) {
+            ans.add(sub.toString());
             return;
         }
-        for(int x=0;x<4;x++)
-        {
-            if(digits.charAt(i) != '7' && digits.charAt(i) != '9' && x==3)
-            break;
 
-            
 
-            int a=Integer.parseInt(String.valueOf(digits.charAt(i))); 
-            sub=sub+(st[a][x]);
-            find(digits, i+1, st, ans, sub);
-            sub = sub.substring(0,sub.length()-1);
+        Character c = digits.charAt(i);
+        String letter = letters.get(c - '0');
+        for (int j = 0; j < letter.length(); j ++) {
+            sub.append(letter.charAt(j));
+            find(digits, i + 1 ,ans, sub);
+            sub.deleteCharAt(sub.length() -  1);
         }
-        
     }
 }
