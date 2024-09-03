93b47aef2d1c6326f67a45efd6e7cd881e432b68
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-03 18:23:27 +0530

    Time: 1 ms (64.31%), Space: 41.7 MB (17.11%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minAddToMakeValid(String s) 
     {
-        int sum =0;
-        int ans=0;
-        int open =0,close =0;
+        int sum =0,ans=0;
         sum = s.charAt(0)=='(' ? 1 : -1;
         for(int i=1;i<s.length();i++)
         {
             if(s.charAt(i) == '(' && s.charAt(i-1) == ')')
             {
-                ans=(sum>=0)? ans : ans+(int)Math.abs(sum)  ;
+                ans= (sum>=0)? ans : ans+(int)Math.abs(sum)  ;
                 sum = sum>0 ? sum : 0;
             }
             if(s.charAt(i) == '(')
@@ -17,7 +15,7 @@ class Solution {
             else
             sum-=1;
         }
-        ans =ans + (int)Math.abs(sum);
-        return ans;
+        return ans + (int)Math.abs(sum);
+        
     }
 }
