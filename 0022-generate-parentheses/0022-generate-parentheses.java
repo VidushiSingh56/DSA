3b4fe568b8d0548f504d0700667d2422f41b2389
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-04 21:38:52 +0530

    Time: 2 ms (55.29%), Space: 43 MB (74.71%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,27 +1,31 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
+    public List<String> generateParenthesis(int n) 
+    {
         List<String> ans = new ArrayList<>();
-        StringBuilder sub = new StringBuilder();
+        String sub="";
         gen(0, 0, sub, ans, n);
         return ans;
+        
     }
-
-    public void gen(int o, int c, StringBuilder sub, List<String> ans, int n) {
-        if (o == n && c == n) {
-            ans.add(sub.toString());
+    public void gen(int o, int c, String sub, List<String> ans,int n)
+    {
+        if(o==n && c==n)
+        {
+            
+                ans.add(sub);
             return;
+            
+            
+            
         }
-        
         if (o < n) {
-            sub.append('(');
-            gen(o + 1, c, sub, ans, n);
-            sub.deleteCharAt(sub.length() - 1); // backtrack
+            gen(o + 1, c, sub + '(', ans, n);
         }
-        
+
         if (c < o) {
-            sub.append(')');
-            gen(o, c + 1, sub, ans, n);
-            sub.deleteCharAt(sub.length() - 1); // backtrack
+            gen(o, c + 1, sub + ')', ans, n);
         }
+
+
     }
 }
