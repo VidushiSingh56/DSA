a3cd30825759ff6edcaa93eceff5958a448e393d
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-10 20:44:18 +0530

    Time: 17 ms (48.97%), Space: 46.3 MB (25.78%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -6,34 +6,44 @@ class Solution {
         Stack<Integer> stack = new Stack <>();
         for(int i=2*n-1;i>=0;i--)
         {
-            if(stack.isEmpty())
-            {
-                // h[i]=-1;
-                stack.push(nums[i%n]);
-            }
-            else if(!stack.isEmpty() && stack.peek()>nums[i%n])
-            {
-                if(i<n)
-                h[i] = stack.peek();
-                stack.push(nums[i%n]);
-            }
-            else
-            {
+
+            // if(stack.isEmpty())
+            // {
+            //     // h[i]=-1;
+            //     stack.push(nums[i%n]);
+            // }
+            // else if(!stack.isEmpty() && stack.peek()>nums[i%n])
+            // {
+            //     if(i<n)
+            //     h[i] = stack.peek();
+            //     stack.push(nums[i%n]);
+            // }
+            // else
+            // {
+            //     while(!stack.isEmpty() && stack.peek()<=nums[i%n])
+            //     {
+            //         stack.pop();
+            //     }
+            //     if(stack.isEmpty())
+            //     {
+            //         if(i<n)
+            //         h[i]=-1;
+            //         // h[i]=(i<n) ? -1 : h[i];
+            //         stack.push(nums[i%n]);continue;
+            //     }
+            //     if(i<n)
+            //     h[i] = stack.peek();
+            //     stack.push(nums[i%n]);
+            // }
                 while(!stack.isEmpty() && stack.peek()<=nums[i%n])
                 {
                     stack.pop();
                 }
-                if(stack.isEmpty())
-                {
-                    if(i<n)
-                    h[i]=-1;
-                    // h[i]=(i<n) ? -1 : h[i];
-                    stack.push(nums[i%n]);continue;
-                }
                 if(i<n)
-                h[i] = stack.peek();
+                {
+                    h[i] = stack.isEmpty() ? -1 : stack.peek();
+                }
                 stack.push(nums[i%n]);
-            }
         }
         return h;
     }    
