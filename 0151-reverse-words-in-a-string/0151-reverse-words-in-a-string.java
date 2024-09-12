bdb3ae5d37667ff3555f1567e0147ce5b0565771
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-13 01:26:05 +0530

    Time: 5 ms (92.8%), Space: 43.4 MB (45.49%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,25 +1,41 @@
 class Solution {
     public String reverseWords(String s) 
     {
-        
-        s = s.trim();
-        int n = s.length();
-        System.out.println(s);
-        int i = n-1;
-        int j = n;
-        String ans = "";
-        while(i>=0)
-        {
-            char ch = s.charAt(i);
+        // s = s.trim();
+        // int n = s.length();
+        // System.out.println(s);
+        // int i = n-1;
+        // int j = n;
+        // String ans = "";
+        // while(i>=0)
+        // {
+        //     char ch = s.charAt(i);
 
-            if(ch == ' ' && s.charAt(i-1) != ' ')
+        //     if(ch == ' ' && s.charAt(i-1) != ' ')
+        //     {
+        //         ans = ans + (s.substring(i+1,j)).trim()+ " ";
+        //         j=i;
+        //     }
+        //     i--;
+        // }
+        // ans = ans + s.substring(i+1,j);
+        // return ans.trim();
+
+         
+        String[] arrays = s.trim().split(" ");
+        StringBuilder string = new StringBuilder();
+        int len = arrays.length;
+        for (int i = 0; i < len; i++) 
+        {
+            if (arrays[len - i - 1].length() != 0) 
             {
-                ans = ans + (s.substring(i+1,j)).trim()+ " ";
-                j=i;
+                string.append(arrays[len - i - 1]);
+                if (i != len - 1) 
+                {
+                    string.append(" ");
+                }
             }
-            i--;
         }
-        ans = ans + s.substring(i+1,j);
-        return ans.trim();
+        return string.toString();
     }
 }
