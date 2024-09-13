a78532a11e5cd72749c55416ab002ae7ba5264fb
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-13 18:34:25 +0530

    Time: 2 ms (100%), Space: 57.6 MB (55.23%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -1,20 +1,39 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) 
     {
-        int n = queries.length;
-        int ans[]=new int[n];
-        for(int i = 0; i<n;i++)
+        int n = arr.length;
+       
+        // for(int i = 0; i<n;i++)
+        // {
+        //     int left = queries[i][0];
+        //     int right = queries[i][1];
+        //     int xor = 0;
+        //     while(left<=right)
+        //     {
+        //         xor = xor ^ arr[left];
+        //         left++;
+        //     }
+        //     ans[i] = xor;
+        // }
+        int N = queries.length;
+        int[] pre = new int[n];
+         int ans[]=new int[N];
+        pre[0] = arr[0];
+        for(int i=1;i<n;i++)
+        {
+            pre[i] = pre[i-1] ^ arr[i];
+        }
+        for(int i =0;i<N;i++)
         {
             int left = queries[i][0];
             int right = queries[i][1];
-            int xor = 0;
-            while(left<=right)
-            {
-                xor = xor ^ arr[left];
-                left++;
-            }
-            ans[i] = xor;
+            if(left == 0)
+            ans[i] = pre[right];
+            else
+            ans[i] = pre[right] ^ pre[left-1];
         }
+
+
         return ans;
     }
 }
