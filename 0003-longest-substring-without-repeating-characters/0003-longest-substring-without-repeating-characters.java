2b369d817313628e24819b5fd7a8c1105e11d535
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-16 00:18:13 +0530

    Time: 108 ms (7.32%), Space: 45.4 MB (10.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,16 +7,13 @@ class Solution {
         while(i<s.length() && j<s.length())
         {
             char ch = s.charAt(j);
-            if(! map.containsKey(ch))
-            {
-                map.put(ch, j);
-            }
-            else
+            if(map.containsKey(ch))
             {
                 i = Math.max(map.get(ch) + 1, i); 
                 System.out.print(i + " ");
                 map.put(ch, j);
             }
+            map.put(ch, j);
             len = j-i+1;
             max = Math.max(max, len);
             j++;
