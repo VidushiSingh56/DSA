80c79afa38e49b8dc066c193e9f87a791d141f2d
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-16 00:17:26 +0530

    Time: 110 ms (7.19%), Space: 45.4 MB (7.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,28 +1,26 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) 
+    {
         HashMap<Character, Integer> map = new HashMap<>();
-        int max = 0;
-        int i = 0, j = 0;
-
-        while (j < s.length()) {
+        int len = 0, max=0;
+        int i =0, j=0;
+        while(i<s.length() && j<s.length())
+        {
             char ch = s.charAt(j);
-
-            if (map.containsKey(ch)) {
-                // Update the left pointer 'i' only if it's behind or at the last occurrence of 'ch'
-                i = Math.max(i, map.get(ch) + 1);
+            if(! map.containsKey(ch))
+            {
+                map.put(ch, j);
             }
-
-            // Update the current character's index
-            map.put(ch, j);
-
-            // Calculate the current length and update max if necessary
-            int len = j - i + 1;
+            else
+            {
+                i = Math.max(map.get(ch) + 1, i); 
+                System.out.print(i + " ");
+                map.put(ch, j);
+            }
+            len = j-i+1;
             max = Math.max(max, len);
-
-            // Move the right pointer forward
             j++;
         }
-
         return max;
     }
 }
