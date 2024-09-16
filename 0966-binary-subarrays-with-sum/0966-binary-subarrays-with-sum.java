150929a68e6ae1c0818ad2ba3c262fd62d70c82d
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-09-16 19:47:27 +0530

    Time: 618 ms (20.64%), Space: 45.2 MB (99.89%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) 
+    {
+        return function(nums,goal)-function(nums, goal-1);
+    }
+    public int function(int nums[], int goal)
+    {
+        if(goal<0)
+        return 0;
+
+        int right =0, left =0, count=0,sum=0;
+        while(right<nums.length)
+        {
+            sum = sum+nums[right];
+            System.out.print(sum+" ");
+            while(sum>goal)
+            {
+                sum-=nums[left];
+                left++;
+            }
+            count=count+right-left+1;
+            right++;
+        }
+        System.out.println();
+        return count;
+
+    }
+}
