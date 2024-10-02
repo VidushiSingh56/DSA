d5dd20435f76a8e4bf595a11ccfbc0330f7a6824
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-10-03 01:17:59 +0530

    Time: 54 ms (13.54%), Space: 61.2 MB (77%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -2,19 +2,41 @@ import java.util.*;
 
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        Map<Integer, Integer> valueToRank = new HashMap<>();  // Map to store value-to-rank mapping
-        int[] sortedUniqueNumbers = Arrays.stream(arr).distinct().sorted().toArray();  // Remove duplicates and sort
+        Map<Integer, Integer> map = new HashMap<>();  // Map to store value-to-rank mapping
+        int[] sorted = Arrays.stream(arr).distinct().sorted().toArray();  // Remove duplicates and sort
         
         // Assign ranks to sorted unique elements
-        for (int i = 0; i < sortedUniqueNumbers.length; i++) {
-            valueToRank.put(sortedUniqueNumbers[i], i + 1);
+        for (int i = 0; i < sorted.length; i++) {
+            map.put(sorted[i], i + 1);
         }
 
         // Replace each element in the original array with its rank
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = valueToRank.get(arr[i]);
+            arr[i] = map.get(arr[i]);
         }
 
         return arr;  // Return the updated array
+
+        // int n = arr.length;
+        // int ans[] = new int[n];
+        // List<Integer> hash = new ArrayList<>();
+        // Arrays.fill(ans, 1);
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(!hash.contains(arr[i]))
+        //     hash.add(arr[i]);
+        // }
+        // int size = hash.size();
+        // for(int i =0;i<n;i++)
+        // {
+        //     for(int j=0;j<size;j++)
+        //     {
+        //         if(arr[i]>hash.get(j))
+        //         {
+        //             ans[i]++;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 }
