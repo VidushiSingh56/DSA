5799fa942266a4ab453722f92fb0d9d445761945
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-10-06 17:44:26 +0530

    Time: 3 ms (83.11%), Space: 42.4 MB (54.66%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Tuple {
+    TreeNode node;
+    int row;
+    int col;
+    
+    public Tuple(TreeNode _node, int _row, int _col) {
+        node = _node;
+        row = _row;
+        col = _col;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Queue<Tuple> que = new LinkedList<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        
+        que.offer(new Tuple(root, 0, 0));
+
+        while (!que.isEmpty()) {
+            Tuple tuple = que.poll();
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+
+            map.putIfAbsent(x, new TreeMap<>());
+            map.get(x).putIfAbsent(y, new PriorityQueue<>());
+            map.get(x).get(y).offer(node.val);
+
+            if (node.left != null) {
+                que.offer(new Tuple(node.left, x - 1, y + 1));
+            }
+            if (node.right != null) {
+                que.offer(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            List<Integer> colList = new ArrayList<>();
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    colList.add(nodes.poll());
+                }
+            }
+            list.add(colList);
+        }
+        return list;
+    }
+}
