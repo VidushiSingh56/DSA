911258dc3ee960268d7f80fd8183c8b554704ceb
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2024-10-08 00:52:06 +0530

    Time: 5 ms (65.3%), Space: 43.1 MB (71.14%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,21 +1,36 @@
 class Solution {
     public int minLength(String s) 
     {
-        String s1 = "AB", s2="CD";
-        int i =0,j=2;
-        while(j<=s.length())
-        {
-            String demo = s.substring(i,j);
-            if(s1.equals(demo) || s2.equals(demo))
-            {
-                s = s.substring(0,i)+s.substring(j); 
-                System.out.print(s+"p ");
-                i=0;
-                j=2;
-                continue;
+        // String s1 = "AB", s2="CD";
+        // int i =0,j=2;
+        // while(j<=s.length())
+        // {
+        //     String demo = s.substring(i,j);
+        //     if(s1.equals(demo) || s2.equals(demo))
+        //     {
+        //         s = s.substring(0,i)+s.substring(j); 
+        //         System.out.print(s+"p ");
+        //         i=0;
+        //         j=2;
+        //         continue;
+        //     }
+        //     i++;j++;
+        // }
+        // return s.length();
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            stack.push(c);
+            boolean op = true;
+            while (stack.size() > 1 && op) {
+                char top = stack.pop();
+                if (stack.peek() == 'A' && top == 'B') stack.pop();
+                else if (stack.peek() == 'C' && top == 'D') stack.pop();
+                else {
+                    stack.push(top);
+                    op = false;
+                }
             }
-            i++;j++;
         }
-        return s.length();
+        return stack.size();
     }
 }
