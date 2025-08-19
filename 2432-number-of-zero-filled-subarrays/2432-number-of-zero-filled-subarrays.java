714620be89c2aa904b68152bca1fc9e09f71be36
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2025-08-19 18:08:08 +0530

    Time: 3 ms (94.03%), Space: 61.9 MB (34.02%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,56 +1,56 @@
 class Solution {
-    // public long zeroFilledSubarray(int[] nums) 
-    // {
-    //     int n = nums.length;
-    //     int flag = 0, count = 0; 
-    //     long sum = 0;
-    //     for(int i = 0; i < n;i++)
-    //     {
-    //         if(nums[i] != 0)
-    //         {
-    //             sum += (int)(flag == 1 ? findsum(count) : 0);
-    //             flag = 0;
-    //             count = 0;
-    //             continue;
-    //         }
-            
-    //         flag = 1;
-    //         count++; 
-    //     }
-    //     if(flag == 1)
-    //     {
-    //         sum += findsum(count);
-    //     }
-    //     return sum;
-    // }
-    // public int findsum(int a)
-    // {
-    //     int sum = 0;
-    //     for(int i = 1; i<=a;i++)
-    //     {
-    //         sum = sum + i;
-    //     }
-    //     return sum;
-    // }
-
-
-    public long zeroFilledSubarray(int[] nums) {
+    public long zeroFilledSubarray(int[] nums) 
+    {
+        int n = nums.length;
+        long flag = 0, count = 0; 
         long sum = 0;
-        long count = 0;
-
-        for (int num : nums) 
+        for(int i = 0; i < n;i++)
         {
-            if (num == 0) 
+            if(nums[i] != 0)
             {
-                count++;          
-                sum += count;     
-            } 
-            else 
-            {
-                count = 0;       
+                sum += (flag == 1 ? findsum(count) : 0);
+                flag = 0;
+                count = 0;
+                continue;
             }
+            
+            flag = 1;
+            count++; 
         }
-
+        if(flag == 1)
+        {
+            sum += findsum(count);
+        }
         return sum;
     }
+    public long findsum(long a)
+    {
+        long sum = 0;
+        for(long i = 1; i<=a;i++)
+        {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+
+    // public long zeroFilledSubarray(int[] nums) {
+    //     long sum = 0;
+    //     long count = 0;
+
+    //     for (int num : nums) 
+    //     {
+    //         if (num == 0) 
+    //         {
+    //             count++;          
+    //             sum += count;     
+    //         } 
+    //         else 
+    //         {
+    //             count = 0;       
+    //         }
+    //     }
+
+    //     return sum;
+    // }
 }
