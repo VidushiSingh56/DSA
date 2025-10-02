bc2f6062d193ba54f0bf9675f75f443acd882f73
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2025-10-02 18:39:08 +0530

    Time: 2 ms (98.67%), Space: 55 MB (79.87%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
@@ -1,24 +1,26 @@
-class Solution {
+class Solution 
+{
     public int maxScore(int[] cardPoints, int k) 
     {
         int n = cardPoints.length;
-        
-        // Calculate the initial sum of the first k cards from the left
-        int currentSum = 0;
-        for (int i = 0; i < k; i++) {
-            currentSum += cardPoints[i];
+        int sum = 0, max = 0;
+        for(int i = 0;i<k;i++)
+        {
+            sum += cardPoints[i];
         }
-        
-        // Initialize maxScore with the current sum of the first k cards
-        int maxScore = currentSum;
-        
-        // Now try to slide from the right end
-        // Take cards from the right and remove from the left
-        for (int i = 0; i < k; i++) {
-            currentSum = currentSum - cardPoints[k - 1 - i] + cardPoints[n - 1 - i];
-            maxScore = Math.max(maxScore, currentSum);
+
+        max=Math.max(max, sum);
+        if(k==n)
+        return max;
+        int j = k-1, l=1;
+        while(j>=0)
+        {
+            sum -= cardPoints[j];
+            j--;
+            sum += cardPoints[n-l];
+            l++;
+            max = Math.max(sum, max);
         }
-        
-        return maxScore;
+        return max;
     }
 }
