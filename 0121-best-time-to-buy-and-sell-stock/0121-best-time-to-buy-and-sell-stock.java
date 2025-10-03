6a6aad739f5f860ba99f8b1235bcfba7f2f201c2
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2025-10-03 21:16:10 +0530

    Time: 1 ms (99.83%), Space: 62.5 MB (7.4%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,22 @@
-class Solution {
+class Solution 
+{
     public int maxProfit(int[] prices) 
     {
         
-        int n=prices.length;
-        int mini=prices[0];
-        int profit=0;
-        for(int i=1;i<n;i++)
+        //We can maintain a minimum from the start of the array and compare it with every element of the array, ifit is greater than the minimum then take the difference and maintain it in max, otherwise update the minimum.
+
+        int min=Integer.MAX_VALUE, maxprofit = 0;
+        for(int i = 0;i<prices.length;i++)
         {
-            int cost=prices[i]-mini;
-            profit = Math.max(cost,profit);
-            mini=Math.min(prices[i],mini);
+            if(min >= prices[i])
+            {
+                min = prices[i];
+                continue;
+            }
+            
+            maxprofit = Math.max(maxprofit, prices[i]-min);
         }
-        return profit;
+
+        return maxprofit;
     }
 }
