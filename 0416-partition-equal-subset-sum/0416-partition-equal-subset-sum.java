a9937f5542ae85c41ca28267c871baa33ab27b88
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2025-10-09 22:09:50 +0530

    Time: 45 ms (58.8%), Space: 46.6 MB (60.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -12,28 +12,49 @@ class Solution
         int target = sum/2;
         int n = nums.length;
         boolean dp[][] = new boolean[n][target+1];
-        boolean seen[][] = new boolean[n][target+1];
-        boolean ans = canfind(n-1, target, nums, dp, seen);
-        return ans;
+        // boolean seen[][] = new boolean[n][target+1];
+        // boolean ans = canfind(n-1, target, nums, dp, seen);
+        // return ans;
+
+        for(int i = 0;i<n;i++)
+        {
+            dp[i][0] = true;
+        }
+        if(nums[0] == target)
+        dp[0][target] = true;
+
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 1;j<=target;j++)
+            {
+                boolean nonpick = dp[i-1][j];
+                boolean pick = false;
+                if(j >= nums[i])
+                pick = dp[i-1][j-nums[i]];
+
+                dp[i][j] = pick || nonpick;
+            }
+        }
+        return dp[n-1][target];
     }
 
-    public boolean canfind(int ind, int target, int[] nums, boolean[][]dp, boolean[][] seen)
-    {
-        if(target == 0)
-        return true;
+    // public boolean canfind(int ind, int target, int[] nums, boolean[][]dp, boolean[][] seen)
+    // {
+    //     if(target == 0)
+    //     return true;
 
-        if(ind == 0) return nums[0] == target;
+    //     if(ind == 0) return nums[0] == target;
 
-        if(seen[ind][target] == true)
-        return dp[ind][target];
+    //     if(seen[ind][target] == true)
+    //     return dp[ind][target];
 
-        boolean nonpick = canfind(ind-1, target, nums, dp, seen);
-        boolean pick = false;
-        if(target >= nums[ind])
-        pick = canfind(ind-1, target-nums[ind], nums, dp, seen);
+    //     boolean nonpick = canfind(ind-1, target, nums, dp, seen);
+    //     boolean pick = false;
+    //     if(target >= nums[ind])
+    //     pick = canfind(ind-1, target-nums[ind], nums, dp, seen);
 
-        seen[ind][target] = true;
-        dp[ind][target] = pick || nonpick;
-        return dp[ind][target];
-    }
+    //     seen[ind][target] = true;
+    //     dp[ind][target] = pick || nonpick;
+    //     return dp[ind][target];
+    // }
 }
