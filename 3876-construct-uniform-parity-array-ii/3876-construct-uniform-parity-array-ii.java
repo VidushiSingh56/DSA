81e276de352588eeab381e2b3dd4b1b550f9c8c2
Vidushi Singh <121214099+VidushiSingh56@users.noreply.github.com>
2026-09-03 22:50:43 +0530

    Time: 32 ms (10.87%), Space: 118.4 MB (99.28%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,55 +1,18 @@
-class Solution 
- {
-    public boolean uniformArray(int[] nums) 
-    {
-        // record IntPair(int x, int y) {}
-        int prevO = 0;
-        int n = nums.length;
+class Solution {
+    public boolean uniformArray(int[] nums) {
         Arrays.sort(nums);
-        Map<Integer, Integer> map = new HashMap<>(); 
-        int min = Integer.MAX_VALUE;
-        for(int i = 0;i<n;i++)
-        {
-            min = Math.min(min, nums[i]);
-            map.put(nums[i], prevO);
-            if(nums[i] % 2 != 0)
-            prevO++;
-        }
-        if(prevO == 0 || prevO == n)
-        return true;
 
-        for(int i = 0;i<n;i++)
-        {
-            if(min % 2 == 0)
-            {
-                //even
-                if(nums[i] == min)
-                continue;
+        int min = nums[0];
+        int oddBefore = 0;
 
-                if(nums[i] % 2 == 0)
-                {
-                    continue;
-                }
-                else
-                {
-                    int evenA = map.get(nums[i]);
-                    if(evenA < 1)
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 2 != 0) {
+                // Odd element needs a smaller odd element
+                if (min % 2 == 0 && oddBefore == 0 && nums[i] != min)
                     return false;
-                }
-            }
-            else
-            {
-                //odd
-                if(nums[i] % 2 == 0)
-                {
-                    int evenA = map.get(nums[i]);
-                    if(evenA < 1)
-                    return false;
-                }
-                else
-                {
-                continue;
-                }
+
+                oddBefore++;
             }
         }
 
